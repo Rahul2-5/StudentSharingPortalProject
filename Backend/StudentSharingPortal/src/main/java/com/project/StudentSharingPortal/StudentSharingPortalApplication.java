797467bafd8e53b5486553f35c4d92d30e4d1f43
Rahul2-5 <rahultemkar61@gmail.com>
2026-03-10 20:48:17 +0530

    Backend test code added(Rahul)

---
+++ Backend/StudentSharingPortal/src/main/java/com/project/StudentSharingPortal/StudentSharingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.project.StudentSharingPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSharingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSharingPortalApplication.class, args);
+	}
+
+}
---
+++ Backend/StudentSharingPortal/src/test/java/com/project/StudentSharingPortal/StudentSharingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.StudentSharingPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSharingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
